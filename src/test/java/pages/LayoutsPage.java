7d0cba9d3fd9ccc0d72a10ca325cdbab7fbe7d31
Franky092 <maximsidelnikov092@gmail.com>
2024-09-16 18:43:47 +0300

    Feat: добавить проход до АР2 ref: изменить селектор перехода к заданию кз

--- src/test/java/pages/LayoutsPage.java
+++ src/test/java/pages/LayoutsPage.java
@@ -2,13 +2,12 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.*;
 
 // page_url = https://stage.app.platforma.build/portfolio/ea7e476c-2e10-4090-9848-ff3389504266/a9e58ddc-c7fa-4c49-a249-6ea3bc291004/layout
 public class LayoutsPage {
 
-    private final SelenideElement layoutOption = $(".cell__action.ng-star-inserted");
+    private final SelenideElement layoutOption = $$(".cell__action.ng-star-inserted").first();
 
     public void chooseLayoutOption()
     {
