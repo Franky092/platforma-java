8e32dfcc5af2d1da20e5cd75f169b0a0d9d1c56b
Franky092 <maximsidelnikov092@gmail.com>
2024-09-16 18:27:33 +0300

    1

--- src/test/java/pages/ProjectsPage.java
+++ src/test/java/pages/ProjectsPage.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.*;
 // page_url = https://stage.app.platforma.build/portfolio/ea7e476c-2e10-4090-9848-ff3389504266
 public class ProjectsPage {
 
-    private final SelenideElement projectSidelnikov = $x("//div[text()='Сидельников']");
+    private final SelenideElement projectSidelnikov = $x("//div[text()='Проект для тестирования']");
 
 
     public void goToProjectSidelnikov() {
